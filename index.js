9ced98af6468db1577a4beb5460db0bc25ac0c1a
Thomas Watson Steen <w@tson.dk>
2015-08-31 00:18:55 +0200

    Refactor code

--- index.js
+++ index.js
@@ -2,17 +2,27 @@
 
 var nl = /\r?\n/
 
-module.exports = function (headers) {
-  if (typeof headers === 'object') headers = headers._header;
+module.exports = function (str) {
+  str = normalize(str)
 
+  // remove leading start-line (e.g. HTTP/1.1 200 OK)
+  var lines = str.split(nl)
+  if (!~lines[0].indexOf(':')) lines = lines.slice(1)
+
+  return parse(lines)
+}
+
+function normalize (str) {
+  if (str && str._header) str = str._header // extra headers from http.ServerResponse object
+  if (!str || typeof str.toString !== 'function') return ''
+  return str.toString().trim()
+}
+
+function parse (headers) {
   var result = {}
-
-  if (!headers) return result
-
-  headers.trim().split(nl).slice(1).forEach(function (header) {
+  headers.forEach(function (header) {
     var index = header.indexOf(':')
     result[header.substr(0, index).toLowerCase()] = header.substr(index + 1).trim()
   })
-
   return result
 }
