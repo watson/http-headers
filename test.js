d2180b0d488cbb6ce3f4abbe3d19937a69d6231c
Thomas Watson Steen <w@tson.dk>
2015-08-31 00:27:44 +0200

    Use tape for testing

--- test.js
+++ test.js
@@ -1,21 +1,56 @@
 'use strict'
 
-var assert = require('assert')
+var test = require('tape')
 var http = require('http')
-var httpHeaders = require('./index')
+var httpHeaders = require('./')
 
 var hStr = 'HTTP/1.1 200 OK\r\nDate: Tue, 10 Jun 2014 07:29:20 GMT\r\nConnection: keep-alive\r\nTransfer-Encoding: chunked\r\n\r\n'
-var hObj = {
+
+var result = {
   date: 'Tue, 10 Jun 2014 07:29:20 GMT',
   connection: 'keep-alive',
   'transfer-encoding': 'chunked'
 }
 
-assert.deepEqual(httpHeaders(), {})
-assert.deepEqual(httpHeaders(''), {})
-assert.deepEqual(httpHeaders({}), {})
-assert.deepEqual(httpHeaders(new http.ServerResponse({})), {})
-assert.deepEqual(httpHeaders({ _header: undefined }), {})
-assert.deepEqual(httpHeaders({ _header: '' }), {})
-assert.deepEqual(httpHeaders({ _header: hStr }), hObj)
-assert.deepEqual(httpHeaders(hStr), hObj)
+test('no argument', function (t) {
+  t.deepEqual(httpHeaders(), {})
+  t.end()
+})
+
+test('empty string', function (t) {
+  t.deepEqual(httpHeaders(''), {})
+  t.end()
+})
+
+test('empty object', function (t) {
+  t.deepEqual(httpHeaders({}), {})
+  t.end()
+})
+
+test('start-line + header', function (t) {
+  t.deepEqual(httpHeaders(hStr), result)
+  t.end()
+})
+
+test('http.ServerResponse', function (t) {
+  t.test('real http.ServerResponse object', function (t) {
+    var res = new http.ServerResponse({})
+    t.deepEqual(httpHeaders(res), {})
+    t.end()
+  })
+
+  t.test('no _header property', function (t) {
+    t.deepEqual(httpHeaders({ _header: undefined }), {})
+    t.end()
+  })
+
+  t.test('empty string as _header', function (t) {
+    t.deepEqual(httpHeaders({ _header: '' }), {})
+    t.end()
+  })
+
+  t.test('normal _header property', function (t) {
+    t.deepEqual(httpHeaders({ _header: hStr }), result)
+    t.end()
+  })
+})
